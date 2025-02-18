f8bc49ff20d3488c1289ed3adbfc81b27022d05e
Sam Lalli <samlalli@Sams-MacBook-Pro.local>
2025-02-18 16:31:41 -0700

    navbar added

--- kong-ai/components/Navbar.tsx
+++ kong-ai/components/Navbar.tsx
@@ -1,9 +1,49 @@
-import React from 'react'
+"use client";
+
+import { UserButton } from "@clerk/nextjs";
+import Image from "next/image";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import React from "react";
+import kongAI from "../public/KongAI.webp"
+
+const routes = [
+  {
+    name: "Chat",
+    path: "/",
+  },
+  {
+    name: "Profile",
+    path: "/profile",
+  },
+];
 
 function Navbar() {
+  const pathname = usePathname();
+
   return (
-    <div>Navbar</div>
-  )
+    <div className="p-4 flex flex-row justify-between items-center bg-black text-white ">
+      <Link href="/" className=" flex flex-rowitems-center gap-x-2">
+        <Image height="30" src={kongAI} alt="kongAI image"/>
+        <h1 className="text-2xl font-bold font-mono">KongAI</h1>
+      </Link>
+      <div className="flex gap-x-6 text-lg items-center">
+        {routes.map((route, idx) => (
+          <Link
+            key={idx}
+            href={route.path}
+            className={
+              pathname === route.path ? "border-b-2 border-green-700" : ""
+            }
+          >
+            {route.name}
+          </Link>
+        ))}
+
+        <UserButton />
+      </div>
+    </div>
+  );
 }
 
-export default Navbar
+export default Navbar;
