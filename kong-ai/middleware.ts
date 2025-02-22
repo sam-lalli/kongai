20420eb727a73ade7a0f15cbf546170c98637804
Sam Lalli <samlalli@Sams-MacBook-Pro.local>
2025-02-21 17:57:28 -0700

    building userpage

--- kong-ai/middleware.ts
+++ kong-ai/middleware.ts
@@ -2,10 +2,8 @@ import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
 
 const isPublicRoute = createRouteMatcher(
   [
-    '/', 
     '/sign-in(.*)', 
     '/sign-up(.*)', 
-    "/profile", 
     "/api/assistant/create", 
     "/api/thread", 
     "/api/message/create", 
