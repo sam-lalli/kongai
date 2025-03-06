f69d4eb11b8f33cb7abac32c949ff6df7ba897cf
Sam Lalli <samlalli@Sams-MacBook-Pro.local>
2025-03-05 17:26:03 -0700

    starting to create profile component

--- kong-ai/app/(app)/profile/page.tsx
+++ kong-ai/app/(app)/profile/page.tsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import ProfileContainer from "@/components/ProfileContainer";
+import { prismadb } from "@/lib/prismadb";
+import { currentUser } from "@clerk/nextjs/server";
+import React from "react";
 
-function ProfilePage() {
+export default async function ProfilePage() {
+  const user = await currentUser();
+
+  if (!user) {
+    throw new Error("No user");
+  }
+
+  let challengePreferences = await prismadb.challengePreferences.findUnique({
+    where: {
+      userId: user.id,
+    },
+  });
+
+  if (!challengePreferences) {
+    challengePreferences = await prismadb.challengePreferences.create({
+      data: {
+        userId: user.id,
+        challengeId: "EASY",
+      },
+    });
+  }
+
   return (
-    <div>ProfilePage</div>
-  )
+    <div className="max-w-screen-lg m-10">
+      <ProfileContainer challengePreferences={challengePreferences} />
+    </div>
+  );
 }
-
-export default ProfilePage
